70589e43502987f2b9a930b5ec2403cf968c4ce1
Jameela Huq <jameela.huq@gmail.com>
2015-09-12 00:21:33 -0700

    Col and Row are submitted to database

--- Player1/main.js
+++ Player1/main.js
@@ -5,58 +5,44 @@
 $(document).ready(init);
 
 
+
 function init(){
-<<<<<<< HEAD
-  var battleshipFirebaseData = getFirebaseData();
-  var lastMove = sendUpdatedData();
-  $(".startButton").click(displayGameBoard);
-  $(".startGame").on("click", sendUpdatedData);
+  $(".makeBoardButton").click(displayGameBoard);
 
   var gameBoard = new Firebase('https://battleshipgames.firebaseio.com/');
-  var rowClicked = new Firebase('https://battleshipgames.firebaseio.com/row');
-  var colClicked = new Firebase('https://battleshipgames.firebaseio.com/col');
 
-  //var battleshipFirebaseData = getFirebaseData();
-  //var lastMove =
-  //$(".startButton").click(displayGameBoard);
+
   var row;
   var col;
   $(".startGame").on("click", sendUpdatedData);
-  $(".pretendShip").on("click", getPositionofTileClick);
-  getPositionofTileClick();
-
-
-  function sendUpdatedData () {
-    console.log("Did I go to soon >_<!");
-    //var oldBoard = battleshipFirebaseData;
-    gameBoard.child('player1').child(row).child(col).update({ship: false, status: 'pinak'});
-  }
-
 
   $("#myGameBoard").on("click",".tile",selectShipPlace);
-}
-function selectShipPlace(){
-  var tile = $(this);
-  tile.css("background-color","red");
-  //update the database
-}
-function isShipHit(){
 
-  function getPositionofTileClick() {
-    console.log("Button clicked!!");
-    //row = 0;
-    //col = 0;
-    rowClicked.on('value', function(snapshot) {
-      console.log("Row clicked!!" + snapshot.val());
-      row = snapshot.val();
-    });
-
-    colClicked.on('value', function(snapshot) {
-      console.log("Col clicked!!" + snapshot.val());
-      col = snapshot.val();
-    });
-    getUpdatedData(row, col);
+  function sendUpdatedData (row,col) {
+    console.log("Did I go to soon >_<!");
+    //var oldBoard = battleshipFirebaseData;
+    gameBoard.child('player1').child(row).child(col).update({ship: true, status: 'njmk'});
   }
+
+  function selectShipPlace(){
+    var tile = $(this);
+    tile.css("background-color","red");
+    var rowSelected = tile.closest('.row').data('row');
+    var colSelected = tile.data('col');
+    console.log(rowSelected);
+    console.log(colSelected);
+    sendUpdatedData(rowSelected,colSelected);
+  }
+}
+
+
+
+
+
+function isShipHit(){}
+
+
+
 
   function getUpdatedData(row, col){
     console.log("get get get get");
@@ -68,7 +54,6 @@ function isShipHit(){
   }
   //$(".row").on("click",".rowElement",cellClicked);
   //$(".imagePlaced").on("click",".rowElement",cellClicked)
-}
 
 function cellClicked(){
   //when cell clicked 
@@ -86,9 +71,9 @@ function displayGameBoard(){
  var $gameBoard = $('.gameBoard');
  for (var j= 0; j < 10; j++) {
    var rowArray = [];
-   $gameBoard.append('<div class="row"></div>');
+   $gameBoard.append('<div class="row" data-row=' + j +'></div>');
    for (var i = 0; i < 10; i++) {
-     rowArray.push('<div class="tile"/>');
+     rowArray.push('<div class="tile" data-col=' + i + '></div>');
    }
    $gameBoard.find('.row:last').append(rowArray);
  }
@@ -96,12 +81,7 @@ function displayGameBoard(){
 
 
 
-function sendUpdatedData() {
-  var battleshipFirebaseData = getFirebaseData();
-  var oldBoard = battleshipFirebaseData;
-  battleshipFirebaseData.child('player1').child(5).child(7).update({ship: true, status: 'miss'});
-  return oldBoard;
-}
+
 
 
 function getFirebaseData(){
