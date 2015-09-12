56213708214e9ce376f7cbd3972adab019ba2d5b
pinank <pinank_dagli@yahoo.com>
2015-09-11 19:19:03 -0700

    intial file

---
+++ Player1/main.js
@@ -0,0 +1,24 @@
+
+function init(){
+var myDataRef = new Firebase('https://cadesnewtest.firebaseio.com/');
+      $('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13) {
+          var name = $('#nameInput').val();
+          var text = $('#messageInput').val();
+          for(var i =0; i<10;i++){
+          	for(var i =0; i<)
+          }
+
+          myDataRef.set({array1:[]});
+          $('#messageInput').val('');
+        }
+      });
+      myDataRef.on('child_added', function(snapshot) {
+        var message = snapshot.val();
+        displayChatMessage(message.name, message.text);
+      });
+      function displayChatMessage(name, text) {
+        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      };
+  
